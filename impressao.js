27368cd4e687bb901b0d11278d6bc529635f3804
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-03-05 16:25:28 -0300

    Adicionado Título da Etiqueta

--- impressao.js
+++ impressao.js
@@ -6,7 +6,11 @@ var table = document.getElementById("myTable");
 var content = '<tr>';
 for (var i = 1; i < qtd; i++) {
     var num = parseInt(start) + i - 1;
-    content += '<td><p>'+ num + '</p></td>'
+    content += '<td>' +
+        '<div class="uni"><p>Universidade Federal do Sul</p><p> e Sudeste do Pará</p><br/>' +
+        '<p>Migração SIADS</p></div>' +
+        '<p>'+ num + '</p>' +
+        '</td>'
     if ((i !== 0) && (i % 65 === 0)){
         content += '</tr>'
         content += '<tr class="pagebreak">'
@@ -17,5 +21,5 @@ for (var i = 1; i < qtd; i++) {
 }
 content += '</tr>'
 table.innerHTML = content;
-window.print();
+// window.print();
 // 9900000001
